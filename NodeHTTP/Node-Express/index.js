da12019dc56cecde18c824bbc23535d986ba30d5
Savya Sachi V B <savyanio@gmail.com>
2020-10-13 21:05:49 +0530

    Express router

--- NodeHTTP/Node-Express/index.js
+++ NodeHTTP/Node-Express/index.js
@@ -1,12 +1,60 @@
 const express = require('express'),
       http = require('http'),
-      morgan = require('morgan');
+      morgan = require('morgan'),
+      bodyParser = require('body-parser');
 
 const port = 3000,
       hostname = 'localhost';
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end("will send all the dishes to you");
+});
+
+
+app.post('/dishes', (req, res, next) => {
+    res.end("will add dish: " + req.body.name + " with details " + req.body.description );
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.end("Put operation not supported");
+    res.statusCode = 403;
+});
+
+
+app.delete('/dishes', (req, res, next) => {
+    res.end("Deleting all items");
+});
+
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end("will send details of the dish to you" + req.param.dishId);
+});
+
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end("Post operation not allowed on " + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write("updating the dish " + req.params.dishId);
+    res.end("Will update the dish- " + req.body.name + "with details" + req.body.description);
+});
+
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end("Deleting the dish " + req.body.dishId);
+});
 
 app.use(express.static(__dirname + '/public'));
 
