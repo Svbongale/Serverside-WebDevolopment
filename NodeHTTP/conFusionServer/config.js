7bc7fab5c1415aff17d615f01e69d86969a1a57c
Savya Sachi V B <savyanio@gmail.com>
2020-11-24 21:09:05 +0530

    Token for O auth

--- NodeHTTP/conFusionServer/config.js
+++ NodeHTTP/conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongourl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '404013080783045', 
-        clientSecret: '55b9728cff90cd0005b72d64b9270972'
+        clientId: 'Id here', 
+        clientSecret: 'secret Here'
     }
 }
