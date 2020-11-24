e8a4405228a8213d33e568db7a62bbd2a9f7c5e4
Savya Sachi V B <savyanio@gmail.com>
2020-11-24 20:53:06 +0530

    O Auth using Facebook

--- NodeHTTP/conFusionServer/config.js
+++ NodeHTTP/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongourl': 'mongodb://localhost:27017/conFusion'
+    'mongourl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '404013080783045', 
+        clientSecret: '55b9728cff90cd0005b72d64b9270972'
+    }
 }
